9c1d269e1344238862837d0017b8e947e59b7bb6
HateBitNel <necrowkrust@gmail.com>
2020-08-12 15:51:05 +0000

    Merge Master into resources

--- TheMiserables/desktop/src/org/academiadecodigo/gitbusters/desktop/DesktopLauncher.java
+++ TheMiserables/desktop/src/org/academiadecodigo/gitbusters/desktop/DesktopLauncher.java
@@ -2,7 +2,6 @@ package org.academiadecodigo.gitbusters.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import org.academiadecodigo.gitbusters.Game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
