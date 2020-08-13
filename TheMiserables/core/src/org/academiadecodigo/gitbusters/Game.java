a366cb893a6b580f2054c0837488284697e6631b
tiashgo <tiago_f_s_r@hotmail.com>
2020-08-13 03:09:25 +0000

    Added Slave escape movement

--- TheMiserables/core/src/org/academiadecodigo/gitbusters/Game.java
+++ TheMiserables/core/src/org/academiadecodigo/gitbusters/Game.java
@@ -12,7 +12,6 @@ import org.academiadecodigo.gitbusters.characters.Guard;
 import org.academiadecodigo.gitbusters.characters.Hero;
 import org.academiadecodigo.gitbusters.characters.Slave;
 import org.academiadecodigo.gitbusters.map.Map;
-import org.academiadecodigo.gitbusters.map.MapArray;
 import org.academiadecodigo.gitbusters.map.Objects.*;
 
 public class Game extends ApplicationAdapter {
@@ -36,9 +35,13 @@ public class Game extends ApplicationAdapter {
 
     private int counter = 0;
 
+    private boolean levelCompleted = false;
+
     private boolean switchOn = false;
 
-    private boolean movingDown = true;
+    private boolean guardMovingDown = true;
+
+    private boolean slaveEscaped = false;
 
     private GameStatus gameStatus = GameStatus.MAINMENU;
 
@@ -81,7 +84,7 @@ public class Game extends ApplicationAdapter {
 
             hero.setTexture(new Texture(Gdx.files.internal("/Users/codecadet/Desktop/PomBitOS/TheMiserables/core/assets/Hero/RevolutionaryBack.png")));
             float y = hero.getRectangle().y;
-            hero.getRectangle().y += Game.cellSize;
+            hero.getRectangle().y += (float)Game.cellSize / 4;
             for (AbstractMapObject object : map.getObjects()) {
                 if (hero.getRectangle().overlaps(object.getRectangle())) {
                     hero.getRectangle().y = y;
@@ -93,7 +96,7 @@ public class Game extends ApplicationAdapter {
 
             hero.setTexture(new Texture(Gdx.files.internal("/Users/codecadet/Desktop/PomBitOS/TheMiserables/core/assets/Hero/RevolutionaryFront.png")));
             float y = hero.getRectangle().y;
-            hero.getRectangle().y -= Game.cellSize;
+            hero.getRectangle().y -= (float)Game.cellSize / 4;
             for (AbstractMapObject object : map.getObjects()) {
                 if (hero.getRectangle().overlaps(object.getRectangle())) {
                     hero.getRectangle().y = y;
@@ -101,28 +104,32 @@ public class Game extends ApplicationAdapter {
             }
         }
 
-        if (guard.getRectangle().y != Game.cellSize && movingDown){
+        if (guard.getRectangle().y != Game.cellSize && guardMovingDown){
             guard.getRectangle().y -= 2;
-            if(hero.getRectangle().x == guard.getRectangle().x && hero.getRectangle().y < guard.getRectangle().y) {
+            if(hero.getRectangle().x < guard.getRectangle().x + (float)16 && hero.getRectangle().x > guard.getRectangle().x - (float)16 && hero.getRectangle().y < guard.getRectangle().y) {
+                guard.setTexture(new Texture(Gdx.files.internal("/Users/codecadet/Desktop/PomBitOS/TheMiserables/core/assets/Guard/GuardBack.png")));
                 System.exit(420);
             }
         }
 
         if (guard.getRectangle().y == cellSize){
             guard.setTexture(new Texture(Gdx.files.internal("/Users/codecadet/Desktop/PomBitOS/TheMiserables/core/assets/Guard/GuardBack.png")));
-            movingDown = false;
+            guardMovingDown = false;
         }
 
-        if (guard.getRectangle().y != 5 * cellSize && !movingDown){
+        if (guard.getRectangle().y != 5 * cellSize && !guardMovingDown){
             guard.getRectangle().y += 2;
-            if(hero.getRectangle().x == guard.getRectangle().x && hero.getRectangle().y > guard.getRectangle().y) {
+            if(hero.getRectangle().x < guard.getRectangle().x + (float)16 && hero.getRectangle().x > guard.getRectangle().x - (float)16 && hero.getRectangle().y > guard.getRectangle().y) {
+                guard.setTexture(new Texture(Gdx.files.internal("/Users/codecadet/Desktop/PomBitOS/TheMiserables/core/assets/Guard/GuardFront.png")));
                 System.exit(420);
             }
         }
 
+
+
         if (guard.getRectangle().y == 5 * cellSize){
             guard.setTexture(new Texture(Gdx.files.internal("/Users/codecadet/Desktop/PomBitOS/TheMiserables/core/assets/Guard/GuardFront.png")));
-            movingDown = true;
+            guardMovingDown = true;
         }
 
         if (hero.getRectangle().overlaps(guard.getRectangle())){
@@ -134,7 +141,7 @@ public class Game extends ApplicationAdapter {
             hero.setTexture(new Texture(Gdx.files.internal("/Users/codecadet/Desktop/PomBitOS/TheMiserables/core/assets/Hero/RevolutionaryLeft.png")));
             float x = hero.getRectangle().x;
             float y = hero.getRectangle().y;
-            hero.getRectangle().x -= Game.cellSize;
+            hero.getRectangle().x -= (float)Game.cellSize / 4;
             for (AbstractMapObject object : map.getObjects()) {
                 if (hero.getRectangle().overlaps(object.getRectangle())) {
                     hero.getRectangle().x = x;
@@ -142,11 +149,37 @@ public class Game extends ApplicationAdapter {
             }
         }
 
+        if (slave.getRectangle().overlaps(guard.getRectangle())){
+            System.exit(1);
+        }
+
+        if (levelCompleted && slave.getRectangle().y > 5 * Game.cellSize) {
+            slave.getRectangle().y -= 4;
+            /**
+            if (!guardMovingDown && slave.getRectangle().x == guard.getRectangle().x) {
+                System.exit(420);
+            }
+             */
+        }
+
+        if (slave.getRectangle().y == 5 * Game.cellSize && !slaveEscaped) {
+            slave.setTexture(new Texture(Gdx.files.internal("/Users/codecadet/Desktop/PomBitOS/TheMiserables/core/assets/Prisioner/PrisionerRight.png")));
+            slave.getRectangle().x += 4;
+            if (slave.getRectangle().x > 336){
+                slave.getTexture().dispose();
+                Rectangle rectangle = slave.getRectangle();
+                rectangle = null;
+                slaveEscaped = true;
+            }
+        }
+
+        // if ()
+
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
 
             hero.setTexture(new Texture(Gdx.files.internal("/Users/codecadet/Desktop/PomBitOS/TheMiserables/core/assets/Hero/RevolutionaryRight.png")));
             float x = hero.getRectangle().x;
-            hero.getRectangle().x += Game.cellSize;
+            hero.getRectangle().x += (float)Game.cellSize / 4;
             for (AbstractMapObject object : map.getObjects()) {
                 if (hero.getRectangle().overlaps(object.getRectangle())) {
                     hero.getRectangle().x = x;
@@ -166,6 +199,7 @@ public class Game extends ApplicationAdapter {
                         map.getMapArray()[5][11] = '_';
                         map.getMapArray()[6][11] = '_';
                         switchOn = true;
+                        levelCompleted = true;
                     }
                 }
             }
